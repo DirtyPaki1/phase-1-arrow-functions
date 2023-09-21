361fccf90e3f9ac45352cb8156663bff038a68a9
Usman Qidwai <usmanqidwai21@gmail.com>
2023-09-20 21:48:11 -0400

    Completed

---
+++ index.js
@@ -0,0 +1,23 @@
+const divide=()=>{
+    let a = 2000
+    let b =100
+    return a/b
+}
+
+divide()
+
+const square=(x)=>{
+    
+    return x * x
+
+}
+
+square()
+
+const add =()=>{
+    let a = 3
+    let b = 4
+    return a+b
+
+}
+add()
